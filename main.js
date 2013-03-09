a05d2487df21392821af976859aff669ac6feefc
C. Scott Ananian <cscott@cscott.net>
2013-03-09 00:49:07 -0500

    Add simple de/compressor based on fix symbol statistics.
    This is based on the simple_c/simple_d example from rngcod13.zip, and serves
    mostly as a demonstration/test of the range coder.  Also added a reusable
    utility class (Util) and a "dummy" range coder used to test modeling.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define(['./lib/freeze','./lib/RangeCoder'], function(freeze,RangeCoder) {
+define(['./lib/freeze','./lib/RangeCoder','./lib/Simple'], function(freeze,RangeCoder,Simple) {
     'use strict';
     return freeze({
         version: "0.0.1",
-        RangeCoder: RangeCoder
+        RangeCoder: RangeCoder,
+        Simple: Simple
     });
 });
