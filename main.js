7d9db48de3f305d7fa183a253e9f12a58bc24e13
C. Scott Ananian <cscott@cscott.net>
2013-03-09 21:21:09 -0500

    Implement simple range-coded variant of LZJB encoding.
    Instead of using a fixed match flag bit / 2-byte match format, we do some
    simple order 0 arithmetic encoding of the literals, match positions, and
    match lengths.

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define(['./lib/freeze','./lib/RangeCoder','./lib/Simple'], function(freeze,RangeCoder,Simple) {
+define(['./lib/freeze','./lib/Lzjb','./lib/RangeCoder','./lib/Simple'], function(freeze,Lzjb,RangeCoder,Simple) {
     'use strict';
     return freeze({
         version: "0.0.1",
         RangeCoder: RangeCoder,
-        Simple: Simple
+        // compression methods
+        Simple: Simple,
+        Lzjb: Lzjb
     });
 });
