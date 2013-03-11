7ad2b7393a729d193f53df5e5224afa315c587c0
C. Scott Ananian <cscott@cscott.net>
2013-03-11 00:04:53 -0400

    Add adaptive Huffman de/compressor, using Vitter algorithm.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define(['./lib/freeze','./lib/Lzjb','./lib/RangeCoder','./lib/Simple'], function(freeze,Lzjb,RangeCoder,Simple) {
+define(['./lib/freeze','./lib/Huffman','./lib/Lzjb','./lib/RangeCoder','./lib/Simple'], function(freeze,Huffman,Lzjb,RangeCoder,Simple) {
     'use strict';
     return freeze({
         version: "0.0.1",
         RangeCoder: RangeCoder,
         // compression methods
+        Huffman: Huffman,
         Simple: Simple,
         Lzjb: Lzjb
     });
