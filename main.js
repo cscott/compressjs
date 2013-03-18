f8138f054cb58cd9611d588e95afe943cd5bbedb
C. Scott Ananian <cscott@cscott.net>
2013-03-18 16:10:05 -0400

    Export BitStream and Stream APIs.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define(['./lib/freeze','./lib/BWT','./lib/Context1Model','./lib/DefSumModel','./lib/FenwickModel','./lib/MTFModel','./lib/NoModel','./lib/Huffman','./lib/RangeCoder','./lib/Dmc','./lib/Lzjb','./lib/Lzp3','./lib/PPM','./lib/Simple'], function(freeze,BWT,Context1Model,DefSumModel,FenwickModel,MTFModel,NoModel,Huffman,RangeCoder,Dmc,Lzjb,Lzp3,PPM,Simple) {
+define(['./lib/freeze','./lib/BitStream','./lib/Stream','./lib/BWT','./lib/Context1Model','./lib/DefSumModel','./lib/FenwickModel','./lib/MTFModel','./lib/NoModel','./lib/Huffman','./lib/RangeCoder','./lib/Dmc','./lib/Lzjb','./lib/Lzp3','./lib/PPM','./lib/Simple'], function(freeze,BitStream,Stream,BWT,Context1Model,DefSumModel,FenwickModel,MTFModel,NoModel,Huffman,RangeCoder,Dmc,Lzjb,Lzp3,PPM,Simple) {
     'use strict';
     return freeze({
         version: "0.0.1",
+        // APIs
+        BitStream: BitStream,
+        Stream: Stream,
         // transforms
         BWT: BWT,
         // models and coder
