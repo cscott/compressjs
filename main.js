4a260aad4cf04a38912ae249d1cee3cb91433ae0
C. Scott Ananian <cscott@cscott.net>
2013-03-19 13:14:20 -0400

    Add BWTC, a "bzip-alike" de/compressor.
    Uses the same basic algorithms as Bzip2, but uses range encoding instead
    of huffman encoding, and doesn't bother to match the file format. Skips
    the initial RLE step, which the author of Bzip2 admits was a mistake.
    Results in output slightly smaller than Bzip2.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define(['./lib/freeze','./lib/BitStream','./lib/Stream','./lib/BWT','./lib/Context1Model','./lib/DefSumModel','./lib/FenwickModel','./lib/MTFModel','./lib/NoModel','./lib/Huffman','./lib/RangeCoder','./lib/Bzip2','./lib/Dmc','./lib/Lzjb','./lib/Lzp3','./lib/PPM','./lib/Simple'], function(freeze,BitStream,Stream,BWT,Context1Model,DefSumModel,FenwickModel,MTFModel,NoModel,Huffman,RangeCoder,Bzip2,Dmc,Lzjb,Lzp3,PPM,Simple) {
+define(['./lib/freeze','./lib/BitStream','./lib/Stream','./lib/BWT','./lib/Context1Model','./lib/DefSumModel','./lib/FenwickModel','./lib/MTFModel','./lib/NoModel','./lib/Huffman','./lib/RangeCoder','./lib/BWTC','./lib/Bzip2','./lib/Dmc','./lib/Lzjb','./lib/Lzp3','./lib/PPM','./lib/Simple'], function(freeze,BitStream,Stream,BWT,Context1Model,DefSumModel,FenwickModel,MTFModel,NoModel,Huffman,RangeCoder,BWTC,Bzip2,Dmc,Lzjb,Lzp3,PPM,Simple) {
     'use strict';
     return freeze({
         version: "0.0.1",
@@ -17,6 +17,7 @@ define(['./lib/freeze','./lib/BitStream','./lib/Stream','./lib/BWT','./lib/Conte
         Huffman: Huffman,
         RangeCoder: RangeCoder,
         // compression methods
+        BWTC: BWTC,
         Bzip2: Bzip2,
         Dmc: Dmc,
         Lzjb: Lzjb,
--- test/file.js
+++ test/file.js
@@ -26,7 +26,8 @@ ALL_LEVELS=[null, 1, 2, 3, 4, 5, 6, 7, 8, 9];
  {name:"lzjb-style", cmp:compressjs.Lzjb, levels:[9]},
  {name:"lzp3(ish)", cmp:compressjs.Lzp3, levels:[null]},
  {name:"dmc", cmp:compressjs.Dmc, levels:[null]},
- {name:"ppm", cmp:compressjs.PPM, levels:[null]}].forEach(function(compressor) {
+ {name:"ppm", cmp:compressjs.PPM, levels:[null]},
+ {name:"bwtc", cmp:compressjs.BWTC, levels:ALL_LEVELS}].forEach(function(compressor) {
      describe(compressor.name+" round-trip encode/decode", function() {
          compressor.levels.forEach(function(level) {
              var desc = (level===null) ? 'default' : ('-'+level);
