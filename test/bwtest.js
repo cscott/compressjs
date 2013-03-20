66ff974f799713f7745fab8a15b41f4ca4455f27
C. Scott Ananian <cscott@cscott.net>
2013-03-20 16:21:36 -0400

    Add testcases for BWT, inspired by jbzip2; fix cyclic wraparound issue.
    There are two different definitions for the BWT.  The traditional one
    assumes that the string wraps around (ie, each row in the matrix is a
    cyclic permutation).  However, it's easier to use a suffix array algorithm
    to compute a version of the BWT where the input string is conceptually
    terminated with a "lower than any other character" marker.  This effectively
    prevents wraparound from influencing the output.
    
    Bzip2 uses the cyclic definition.  I implemented a suffix-array based BWT
    algorith, so it uses the non-cyclic definition.
    
    Thanks to http://stackoverflow.com/a/13983026/2192668 I hacked the
    suffix-array based BWT to generate the cyclic sort, but at the cost
    of doubling the size of the input array.  That's unfortunate.

--- test/bwtest.js
+++ test/bwtest.js
@@ -24,7 +24,7 @@ var testFile = function(filename, verbose) {
     if (verbose) { console.log((finish - start)/1000, 'seconds'); }
 
     if (verbose) { console.log('unbwtcheck...'); }
-    BWT.unbwt(U, V, A, n, pidx);
+    BWT.unbwtransform(U, V, A, n, pidx);
     for (i=0; i<n; i++) {
         assert.ok(T[i] === V[i],
                   'mismatch at', i,': ',T[i],'!=',V[i]);
@@ -35,6 +35,59 @@ var testFile = function(filename, verbose) {
 };
 
 describe('BWT transform', function() {
+    var testcases = [
+    {
+        description: "Stack overflow example",
+        input:  "bcababa",
+        output: "cbbaaab",
+        index:  5
+    },{
+        description: "Alphabet",
+        input:  "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        output: "ZABCDEFGHIJKLMNOPQRSTUVWXY",
+        index: 0
+    },{
+        description: "Reversed alphabet",
+        input:  "ZYXWVUTSRQPONMLKJIHGFEDCBA",
+        output: "BCDEFGHIJKLMNOPQRSTUVWXYZA",
+        index: 25
+    },{
+        description: "Wikipedia example.",
+        input:  'SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES',
+        output: 'TEXYDST.E.IXIXIXXSSMPPS.B..E.S.EUSFXDIIOIIIT',
+        index: 29
+    },{
+        description: "jbzip2 test case",
+        input: ("Mary had a little lamb, its fleece was white as snow" +
+                "Mary had a little lamb, its fleece was white as snow" +
+                "Mary had a little lamb, its fleece was white as snow" +
+                "Mary had a little lamb, its fleece was white as snow" +
+                "Mary had a little lamb, its fleece was white as snow" +
+                "Mary had a little lamb, its fleece was white as snow" +
+                "Mary had a little lamb, its fleece was white as snow" +
+                "Mary had a little lamb, its fleece was white as snow" +
+                // break symmetry so we can distinguish rotations
+                "Nary had a little lamb, its fleece was white as snow"),
+        output: ("dddddddddeeeeeeeeesssssssssyyyyyyyyy,,,,,,,,,eeeeeeeeeaaaaaaaaassssssssseeeeeeeeesss" +
+                 "ssssssbbbbbbbbbwwwwwwwww         hhhhhhhhhlllllllllNMMMMMMMM         wwwwwwwwwmmmmmm" +
+                 "mmmeeeeeeeeeaaaaaaaaatttttttttlllllllllccccccccceeeeeeeeelllllllll                  " +
+                 "wwwwwwwwwhhhhhhhhh         lllllllll         tttttttttfffffffff         aaaaaaaaasss" +
+                 "ssssssnnnnnnnnnaaaaaaaaatttttttttaaaaaaaaaaaaaaaaaa         iiiiiiiiitttttttttiiiiii" +
+                 "iiiiiiiiiiiiooooooooo                  rrrrrrrrr"),
+        index: 99
+    }];
+
+    testcases.forEach(function(t) {
+        it('should correctly sort '+t.description, function() {
+            var T = new Buffer(t.input, 'ascii');
+            var U = new Buffer(T.length);
+            var pidx = BWT.bwtransform2(T, U, T.length, 256);
+            var s2 = U.toString('ascii');
+            assert.equal(s2, t.output);
+            assert.equal(pidx, t.index);
+        });
+    });
+
     ['sample0', 'sample1', 'sample2', 'sample3', 'sample4', 'sample5'].forEach(function(f) {
         it('should correctly round-trip '+f, function() {
             testFile(f, false);
