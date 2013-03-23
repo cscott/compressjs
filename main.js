3f550bd2f43449e3163869ec78d596ba519ea661
C. Scott Ananian <cscott@cscott.net>
2013-03-23 00:06:38 -0400

    Add unmodified LZJB algorithm.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define(['./lib/freeze','./lib/BitStream','./lib/Stream','./lib/BWT','./lib/Context1Model','./lib/DefSumModel','./lib/FenwickModel','./lib/MTFModel','./lib/NoModel','./lib/Huffman','./lib/RangeCoder','./lib/BWTC','./lib/Bzip2','./lib/Dmc','./lib/LzjbR','./lib/Lzp3','./lib/PPM','./lib/Simple'], function(freeze,BitStream,Stream,BWT,Context1Model,DefSumModel,FenwickModel,MTFModel,NoModel,Huffman,RangeCoder,BWTC,Bzip2,Dmc,LzjbR,Lzp3,PPM,Simple) {
+define(['./lib/freeze','./lib/BitStream','./lib/Stream','./lib/BWT','./lib/Context1Model','./lib/DefSumModel','./lib/FenwickModel','./lib/MTFModel','./lib/NoModel','./lib/Huffman','./lib/RangeCoder','./lib/BWTC','./lib/Bzip2','./lib/Dmc','./lib/Lzjb','./lib/LzjbR','./lib/Lzp3','./lib/PPM','./lib/Simple'], function(freeze,BitStream,Stream,BWT,Context1Model,DefSumModel,FenwickModel,MTFModel,NoModel,Huffman,RangeCoder,BWTC,Bzip2,Dmc,Lzjb,LzjbR,Lzp3,PPM,Simple) {
     'use strict';
     return freeze({
         version: "0.0.1",
@@ -20,6 +20,7 @@ define(['./lib/freeze','./lib/BitStream','./lib/Stream','./lib/BWT','./lib/Conte
         BWTC: BWTC,
         Bzip2: Bzip2,
         Dmc: Dmc,
+        Lzjb: Lzjb,
         LzjbR: LzjbR,
         Lzp3: Lzp3,
         PPM: PPM,
